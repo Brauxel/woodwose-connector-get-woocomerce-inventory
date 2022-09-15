7e6b6a96fec64fc067b6ce7cb6777d79ecb3f218
Aakash Bhatia <Aakash.Bhatia@vicinity.com.au>
2022-09-15 19:50:02 +1000

    run scripts added

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import { createWooCommerceApi } from './utils/wooCommerce/createWooCommerceApi'
+
+process.on('uncaughtException', (err) => {
+  console.error('There was an uncaught error', err)
+  process.exit(1) //mandatory (as per the Node.js docs)
+})
+
+createWooCommerceApi()
