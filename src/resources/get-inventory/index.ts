e61ac89de6726497e9455f9aece9aa7f560a465f
Aakash Bhatia <Aakash.Bhatia@vicinity.com.au>
2022-09-17 14:58:28 +1000

    create woocommerce api function

---
+++ src/resources/get-inventory/index.ts
@@ -0,0 +1,3 @@
+export const getWooCommerceInventory = async () => {
+  console.log('getWooCommerceInventory')
+}
--- src/utils/woocommerce/createWooCommerceApi.ts
+++ src/utils/woocommerce/createWooCommerceApi.ts
@@ -1,4 +1,21 @@
-export const createWooCommerceApi = () => {
-  const value = 'sadasd'
-  console.log('helloww', value)
+import WooCommerceRestApi from '@woocommerce/woocommerce-rest-api'
+
+export const createWooCommerceApi = (
+  url = process.env.WORDPRESS_URL as string,
+  consumerKey = process.env.WOOCOMMERCE_CONSUMER_KEY as string,
+  consumerSecret = process.env.WOOCOMMERCE_CONSUMER_SECRET as string
+) => {
+  try {
+    return new WooCommerceRestApi({
+      url,
+      consumerKey,
+      consumerSecret,
+      version: 'wc/v3',
+    })
+  } catch (error) {
+    // return logAndThrowError(
+    //   "Could not create Woo Commerce Api",
+    //   error as Error
+    // );
+  }
 }
