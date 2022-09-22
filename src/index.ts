c3b09505530fdb2997740b40c703e48c6b6b3015
Aakash Bhatia <Aakash.Bhatia@vicinity.com.au>
2022-09-22 12:19:48 +1000

    working on lambda

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,18 @@
 import { fetchAndCreateWooCommerceData } from './resources/get-inventory'
-import { hydrateEnv } from './utils/hydrators/secrets'
+// import { hydrateEnv } from './utils/hydrators/secrets'
 
 process.on('uncaughtException', (err) => {
   console.error('There was an uncaught error', err)
   process.exit(1) //mandatory (as per the Node.js docs)
 })
 
-const start = async () => {
-  await hydrateEnv()
+export const handler = async () => {
+  // await hydrateEnv()
   const data = await fetchAndCreateWooCommerceData()
-  return data
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      data,
+    }),
+  }
 }
-
-start()
