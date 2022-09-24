dc2092233f6ca281a498f2d643f82c5e6b92b89b
Aakash Bhatia <Aakash.Bhatia@vicinity.com.au>
2022-09-24 19:22:07 +1000

    holder

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,15 @@
-import { fetchAndCreateWooCommerceData } from './resources/get-inventory'
 import { APIGatewayProxyResult } from 'aws-lambda'
-import { hydrateEnv } from './utils/hydrators/secrets'
 
 process.on('uncaughtException', (err) => {
   console.error('There was an uncaught error', err)
   process.exit(1) //mandatory (as per the Node.js docs)
 })
 
-hydrateEnv()
 export const handler = async (): Promise<APIGatewayProxyResult> => {
-  const data = await fetchAndCreateWooCommerceData()
   return {
     statusCode: 200,
     body: JSON.stringify({
-      data,
+      message: 'hello world',
     }),
   }
-}
-
-// If we're developing on our local machine, we run the handler function so we can debug the code
-if (process.env.NODE_ENV === 'local') {
-  handler()
 }
